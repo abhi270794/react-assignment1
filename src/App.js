011808131856a2f5e5e60cbd4363ba83e33ac60b
ABHISHEK SONI <86410022+abhi270794@users.noreply.github.com>
2022-01-04 17:34:36 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <div className="App">
+      <h1>Mobile Operating System</h1>
+      <li>Android</li>
+      <li>Blackberry</li>
+      <li>iPhone</li>
+      <li>Windows Phone</li>
+      <h2>Mobile Manufacturers</h2>
+      <li>Samsung</li>
+      <li>HTC</li>
+      <li>Micromax</li>
+      <li>Apple</li>
+    </div>
+    </>
+  );
+}
+
+export default App;
